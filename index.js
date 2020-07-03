85126b2e86f66b53ba40f46fb062b067c36fbdf8
Hîrbu Andrei <andrei_hrb@icloud.com>
2020-07-04 00:08:51 +0300

    delete developer parts

--- index.js
+++ index.js
@@ -23,15 +23,8 @@ app.get("/", async (res, req) => {
   const formatedFuture = dateFormat(future, "yyyy-mm-dd");
 
   (async () => {
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({
-      width: 1920,
-      height: 1080,
-      deviceScaleFactor: 1,
-    });
 
     await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
 
