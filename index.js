98ecdd4a423dd8d89760b903d6967f67803821e1
Hîrbu Andrei <andrei_hrb@icloud.com>
2020-07-04 00:04:59 +0300

    adding express and web server

--- index.js
+++ index.js
@@ -1,118 +1,128 @@
 const nodemailer = require("nodemailer");
 const puppeteer = require("puppeteer");
 const dateFormat = require("dateformat");
-require("dotenv").config();
+const express = require("express");
+// require("dotenv").config();
 
-const location = process.env.LOCATION;
-const emails = process.env.EMAILS.split(",");
-const propreties = process.env.PROPRETIES.split(",");
-const propretiesFind = [];
+const app = express();
+const port = process.env.PORT;
 
-let searchLink = "";
+app.get("/", async (res, req) => {
+  const location = process.env.LOCATION;
+  const emails = process.env.EMAILS.split(",");
+  const propreties = process.env.PROPRETIES.split(",");
+  const propretiesFind = [];
 
-const today = new Date();
-const future = new Date();
-future.setDate(future.getDate() + 2);
+  let searchLink = "";
 
-const formatedToday = dateFormat(today, "yyyy-mm-dd");
-const formatedFuture = dateFormat(future, "yyyy-mm-dd");
+  const today = new Date();
+  const future = new Date();
+  future.setDate(future.getDate() + 2);
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-    deviceScaleFactor: 1,
-  });
+  const formatedToday = dateFormat(today, "yyyy-mm-dd");
+  const formatedFuture = dateFormat(future, "yyyy-mm-dd");
 
-  await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    });
 
-  await page.click("input[data-component]");
-  await page.waitFor(250);
-  await page.keyboard.type(location, { delay: 100 });
-  await page.waitFor(250);
+    await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
 
-  await page.click(".xp__dates");
-  await page.waitFor(250);
-  await page.click(`td[data-date='${formatedToday}']`);
-  await page.waitFor(100);
-  await page.click(`td[data-date='${formatedFuture}']`);
-  await page.waitFor(100);
+    await page.click("input[data-component]");
+    await page.waitFor(250);
+    await page.keyboard.type(location, { delay: 100 });
+    await page.waitFor(250);
 
-  await page.click("button.sb-searchbox__button");
-  await page.waitForNavigation();
-  searchLink = page.url();
+    await page.click(".xp__dates");
+    await page.waitFor(250);
+    await page.click(`td[data-date='${formatedToday}']`);
+    await page.waitFor(100);
+    await page.click(`td[data-date='${formatedFuture}']`);
+    await page.waitFor(100);
 
-  for (let proprety of propreties) {
-    // forEach in async sucks
-    const found = await page.evaluate(
-      (proprety) => window.find(proprety),
-      proprety
-    );
-    propretiesFind.push(await found);
-  }
+    await page.click("button.sb-searchbox__button");
+    await page.waitForNavigation();
+    searchLink = page.url();
 
-  const transporter = nodemailer.createTransport({
-    pool: true,
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: process.env.MAIL_SECURE, // use TLS
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
+    for (let proprety of propreties) {
+      // forEach in async sucks
+      const found = await page.evaluate(
+        (proprety) => window.find(proprety),
+        proprety
+      );
+      propretiesFind.push(await found);
+    }
 
-  await transporter.sendMail({
-    from: `"${process.env.MAIL_NAME}" <${process.env.MAIL_USER}>`,
-    to: emails.join(", "),
-    subject: `${process.env.MAIL_SUBJECT} [${formatedToday}]`,
-    text: getText(propreties, propretiesFind),
-    html: getHtml(propreties, propretiesFind),
-  });
+    const transporter = nodemailer.createTransport({
+      pool: true,
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_SECURE, // use TLS
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
-  await browser.close();
-})();
+    await transporter.sendMail({
+      from: `"${process.env.MAIL_NAME}" <${process.env.MAIL_USER}>`,
+      to: emails.join(", "),
+      subject: `${process.env.MAIL_SUBJECT} [${formatedToday}]`,
+      text: getText(propreties, propretiesFind),
+      html: getHtml(propreties, propretiesFind),
+    });
 
-/**
- * Change these acordigly
- */
-const getText = (propreties, propretiesFind) => {
-  let output = "";
+    await browser.close();
+  })();
 
-  for (let i = 0; i < propreties.length; i++) {
+  /**
+   * Change these acordigly
+   */
+  const getText = (propreties, propretiesFind) => {
+    let output = "";
+
+    for (let i = 0; i < propreties.length; i++) {
+      output +=
+        propreties[i] +
+        " " +
+        (propretiesFind[i] ? "" : "nu") +
+        " este disponibilă" +
+        "\n";
+    }
+
+    return output;
+  };
+
+  const getHtml = (propreties, propretiesFind) => {
+    let output = "";
+
+    for (let i = 0; i < propreties.length; i++) {
+      output +=
+        "<i>" +
+        propreties[i] +
+        "</i>" +
+        " " +
+        (propretiesFind[i] ? "" : "<b>nu</b>") +
+        " <b>este</b> disponibilă" +
+        "<br />";
+    }
+
+    output += `<br />Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLink}">acest link</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!`;
+
     output +=
-      propreties[i] +
-      " " +
-      (propretiesFind[i] ? "" : "nu") +
-      " este disponibilă" +
-      "\n";
-  }
+      '<br /><br /><small>Creat cu multă ❤️ și ☕ de către <a href="https://andrei-hrb.com/" rel="noopener noreferrer" target="_blank">Hîrbu Andrei</a></small>.';
 
-  return output;
-};
+    return output;
+  };
 
-const getHtml = (propreties, propretiesFind) => {
-  let output = "";
+  res.send("Done!");
+});
 
-  for (let i = 0; i < propreties.length; i++) {
-    output +=
-      "<i>" +
-      propreties[i] +
-      "</i>" +
-      " " +
-      (propretiesFind[i] ? "" : "<b>nu</b>") +
-      " <b>este</b> disponibilă" +
-      "<br />";
-  }
-
-  output += `<br />Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLink}">acest link</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!`;
-
-  output +=
-    '<br /><br /><small>Creat cu multă ❤️ și ☕ de către <a href="https://andrei-hrb.com/" rel="noopener noreferrer" target="_blank">Hîrbu Andrei</a></small>.';
-
-  return output;
-};
+app.listen(port, () => console.log(`App listening at port ${port}`));
