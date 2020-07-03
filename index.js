63a4f62553e52762064da6a8c0342977f86f4656
Hîrbu Andrei <andrei_hrb@icloud.com>
2020-07-04 00:12:32 +0300

    circumvent detection

--- index.js
+++ index.js
@@ -23,7 +23,25 @@ app.get("/", async (res, req) => {
   const formatedFuture = dateFormat(future, "yyyy-mm-dd");
 
   (async () => {
-    const browser = await puppeteer.launch();
+    // prevent detecting Pu
+    const args = [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-infobars",
+      "--window-position=0,0",
+      "--ignore-certifcate-errors",
+      "--ignore-certifcate-errors-spki-list",
+      '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"',
+    ];
+
+    const options = {
+      args,
+      headless: true,
+      ignoreHTTPSErrors: true,
+      userDataDir: "./tmp",
+    };
+
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
     await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
