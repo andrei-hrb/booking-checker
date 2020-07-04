656d62349827c0a2b550a0ce848f1c1e6c9cf381
Hîrbu Andrei <andrei_hrb@icloud.com>
2020-07-04 09:30:46 +0300

    Fix email

--- index.js
+++ index.js
@@ -124,15 +124,15 @@ const getHtml = (propreties, propretiesFind) => {
       "<br />";
   }
 
-  output += `<br />Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLink}">acest link</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!`;
-
   output += "<br /><br /><hr />";
 
-  output +=
-    "<br /><small>Nu răspundeți acestui email/trimiteți email-uri pe acestă adresă, nu vă va răspunde nimeni.</small>.";
+  output += `<br /><br /><small>Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLink}">acest link</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!</small>`;
 
   output +=
-    '<br /><small>Creat cu multă ❤️ și ☕ de către <a href="https://andrei-hrb.com/" rel="noopener noreferrer" target="_blank">Hîrbu Andrei</a></small>.';
+    "<br /><br /><small>Nu răspundeți acestui email/trimiteți email-uri pe acestă adresă, nu vă va răspunde nimeni.</small>";
+
+  output +=
+    '<br /><br /><small>Creat cu multă ❤️ și ☕ de către <a href="https://andrei-hrb.com/" rel="noopener noreferrer" target="_blank">Hîrbu Andrei</a>.</small>';
 
   return output;
 };
