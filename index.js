202c22bb7bdf3850f2eb93a32012eee5f3255cd6
Hîrbu Andrei <andrei_hrb@icloud.com>
2020-07-15 11:50:03 +0300

    Adding tommorow finding of availability

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const express = require("express");
 const location = process.env.LOCATION;
 const emails = process.env.EMAILS.split(",");
 const propreties = process.env.PROPRETIES.split(",");
-const propretiesFind = [];
+const propretiesFindTommorow = [];
+const propretiesFindFuture = [];
 
-let searchLink = "";
+let searchLinkTommorow = "";
+let searchLinkFuture = "";
 
 const today = new Date();
+const tommorow = new Date();
 const future = new Date();
+tommorow.setDate(tommorow.getDate() + 1);
 future.setDate(future.getDate() + 2);
 
 const formatedToday = dateFormat(today, "yyyy-mm-dd");
+const formatedTommorow = dateFormat(tommorow, "yyyy-mm-dd");
 const formatedFuture = dateFormat(future, "yyyy-mm-dd");
 
 const functionality = async () => {
@@ -44,31 +49,44 @@ const functionality = async () => {
     deviceScaleFactor: 1,
   });
 
-  await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
+  for (let i = 0; i < 2; i++) {
+    await page.goto("https://www.booking.com/", { waitUntil: "networkidle2" });
 
-  await page.click("input[data-component]");
-  await page.waitFor(250);
-  await page.keyboard.type(location, { delay: 100 });
-  await page.waitFor(250);
+    await page.click("input[data-component]");
+    await page.waitFor(250);
+    await page.keyboard.type(location, { delay: 100 });
+    await page.waitFor(250);
 
-  await page.click(".xp__dates");
-  await page.waitFor(250);
-  await page.click(`td[data-date='${formatedToday}']`);
-  await page.waitFor(100);
-  await page.click(`td[data-date='${formatedFuture}']`);
-  await page.waitFor(100);
-
-  await page.click("button.sb-searchbox__button");
-  await page.waitForNavigation();
-  searchLink = page.url();
-
-  for (let proprety of propreties) {
-    // forEach in async sucks
-    const found = await page.evaluate(
-      (proprety) => window.find(proprety),
-      proprety
+    await page.click(".xp__dates");
+    await page.waitFor(250);
+    await page.click(`td[data-date='${formatedToday}']`);
+    await page.waitFor(250);
+    await page.click(
+      `td[data-date='${i === 0 ? formatedTommorow : formatedFuture}']`
     );
-    propretiesFind.push(await found);
+    await page.waitFor(100);
+
+    await page.click("button.sb-searchbox__button");
+    await page.waitForNavigation();
+    searchLinkTommorow = i === 0 ? page.url() : searchLinkTommorow;
+    searchLinkFuture = i === 1 ? page.url() : searchLinkFuture;
+
+    for (let proprety of propreties) {
+      // forEach in async sucks
+      const found = await page.evaluate(
+        (proprety) => window.find(proprety),
+        proprety
+      );
+      if (i === 0) {
+        propretiesFindTommorow.push(await found);
+      } else {
+        propretiesFindFuture.push(await found);
+      }
+    }
+
+    const client = await page.target().createCDPSession();
+    await client.send("Network.clearBrowserCookies");
+    await client.send("Network.clearBrowserCache");
   }
 
   await browser.close();
@@ -87,30 +105,14 @@ const functionality = async () => {
     from: `"${process.env.MAIL_NAME}" <${process.env.MAIL_USER}>`,
     to: emails.join(", "),
     subject: `${process.env.MAIL_SUBJECT} [${formatedToday}]`,
-    text: getText(propreties, propretiesFind),
-    html: getHtml(propreties, propretiesFind),
+    html: getHtml(propreties, propretiesFindTommorow, propretiesFindFuture),
   });
 };
 
 /**
  * Change these acordigly
  */
-const getText = (propreties, propretiesFind) => {
-  let output = "";
-
-  for (let i = 0; i < propreties.length; i++) {
-    output +=
-      propreties[i] +
-      " " +
-      (propretiesFind[i] ? "" : "nu") +
-      " este disponibilă" +
-      "\n";
-  }
-
-  return output;
-};
-
-const getHtml = (propreties, propretiesFind) => {
+const getHtml = (propreties, propretiesFindTommorow, propretiesFindFuture) => {
   let output = "";
 
   for (let i = 0; i < propreties.length; i++) {
@@ -119,14 +121,25 @@ const getHtml = (propreties, propretiesFind) => {
       propreties[i] +
       "</i>" +
       " " +
-      (propretiesFind[i] ? "" : "<b>nu</b>") +
-      " <b>este</b> disponibilă" +
+      (propretiesFindTommorow[i] ? "" : "<b>nu</b>") +
+      " <b>este</b> disponibilă pentru un sejur de o noapte." +
       "<br />";
+
+    output +=
+      "<i>" +
+      propreties[i] +
+      "</i>" +
+      " " +
+      (propretiesFindFuture[i] ? "" : "<b>nu</b>") +
+      " <b>este</b> disponibilă pentru un sejur de două nopți." +
+      "<br />";
+
+    output += "<br />";
   }
 
   output += "<br /><br /><hr />";
 
-  output += `<br /><br /><small>Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLink}">acest link</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!</small>`;
+  output += `<br /><br /><small>Acesta este încă un soft netestat extensiv, așa că pot exista probleme cu acuratețea informațiilor; Recomand, cel puțin pâna la dispariția acestui paragraf, verificarea acestor informații pe Booking.com apasând <a href="${searchLinkTommorow}">acest link pentru un sejur de o noapte</a> sau <a href="${searchLinkFuture}">acest link pentru un sejur de două nopți</a>, iar orice problemă trebuie comunicată. Mulțumesc anticipat!</small>`;
 
   output +=
     "<br /><br /><small>Nu răspundeți acestui email/trimiteți email-uri pe acestă adresă, nu vă va răspunde nimeni.</small>";
@@ -134,6 +147,7 @@ const getHtml = (propreties, propretiesFind) => {
   output +=
     '<br /><br /><small>Creat cu multă ❤️ și ☕ de către <a href="https://andrei-hrb.com/" rel="noopener noreferrer" target="_blank">Hîrbu Andrei</a>.</small>';
 
+  output += "<br /><br /><small>Version: 1.01 - Updated on 15.07.2020</small>";
   return output;
 };
 
